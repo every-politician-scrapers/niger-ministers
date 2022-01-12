9b680bb4a66590226a19040da1e31edd98f45b8c
Tony Bowden <tony@tmtm.com>
2022-01-12 19:59:18 +0200

    compare enwiki Speaker

---
+++ sources/speaker-enwiki/scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  # decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Name'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name start end].freeze
+    end
+
+    def raw_end
+      super.gsub('*', '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
